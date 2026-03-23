ba938fd50f1332f4be5be4740e1fb8be4b457ee2
Oscar Rojas <orojasb@unal.edu.co>
2026-03-23 12:06:28 -0500

    fix: usar psycopg3 en session

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "postgresql+psycopg2://postgres:FEYdeu94@localhost:5432/gamehub_db"
 
-engine = create_engine(DATABASE_URL, echo=True, future=True)
+db_url = DATABASE_URL.replace("postgresql://", "postgresql+psycopg://")
+engine = create_engine(db_url, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 

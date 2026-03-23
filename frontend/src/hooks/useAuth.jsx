35f5196ba70cece6c77a2da3d6826f263f0a7484
Oscar Rojas <orojasb@unal.edu.co>
2026-03-23 17:05:49 -0500

    update url

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -1,7 +1,9 @@
 import { createContext, useContext, useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000';
+// ✅ Bien
+const API_URL = import.meta.env.VITE_API_URL || 'https://my-hub-yc50.onrender.com';
+
 const AuthContext = createContext();
 
 export const useAuth = () => {
